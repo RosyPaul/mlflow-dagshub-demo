e10ec4e25afaed687fe37e3b3d64fc074ceee3d7
Rosy <rosypaul375@gmail.com>
2026-04-30 13:40:54 +0530

    initial commit

---
+++ iris_dt.py
@@ -0,0 +1,54 @@
+import mlflow
+
+# mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+iris=load_iris()
+X=iris.data
+y=iris.target
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state= 42)
+
+max_depth = 10
+
+mlflow.set_experiment('iris-dt')
+with mlflow.start_run():
+    dt=DecisionTreeClassifier(max_depth=max_depth)
+    dt.fit(X_train,y_train)
+    y_pred= dt.predict(X_test)
+
+    accuracy=accuracy_score(y_test,y_pred)
+
+    mlflow.log_metric('accuracy',accuracy)
+    mlflow.log_param('max_depth',max_depth)
+    # mlflow.log_param('n_estimators',n_estimators)
+
+    cm=confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm,annot=True,fmt='d',cmap='Blues',xticklabels=iris.target_names,yticklabels=iris.target_names)
+    plt.ylabel('actutal')
+    plt.xlabel('Predicted')
+    plt.title('COnfusion Matrix')
+
+    plt.savefig("Confusion_matrix")
+
+    mlflow.log_artifact('Confusion_matrix.png')
+
+    mlflow.log_artifact(__file__)
+    # mlflow.sklearn.log_model(dt,"decision tree")
+
+    mlflow.set_tag('author','rosy')
+    
+
+    print('accuracy',accuracy)
+
+
+
